e097cf65893e82bd6b4e4e2fd2de87618e06a4ee
Carlo Amante <carlo@MBP-di-Carlo.lan>
2019-07-21 17:46:34 +0200

    template for friend's list

--- unimitube/profile.php
+++ unimitube/profile.php
@@ -7,13 +7,12 @@
     $sqldata_n="SELECT data_n FROM utente WHERE email='".$_SESSION["email"]."'";
     $sqlsesso="SELECT sesso FROM utente WHERE email='".$_SESSION["email"]."'";
     $sqlfoto="SELECT foto FROM utente WHERE email='".$_SESSION["email"]."'";
-    $resemail=$mysqli->query($sqlemail);
+    $resemail=$mysqli->query($sqlemail) ;
     $resnickname=$mysqli->query($sqlnickname);
     $rescitta_n=$mysqli->query($sqlcitta_n);
     $resdata_n=$mysqli->query($sqldata_n);
     $ressesso=$mysqli->query($sqlsesso);
     $resfoto=$mysqli->query($sqlfoto);
-
     ?>
 <!doctype html>
 <html lang="en">
@@ -65,59 +64,55 @@
     </div>
     <div class="container-fluid">
         <div class="row mb-2">
-            <div class="col-md-3">
-                <img src="<?php while($row = $resfoto->fetch_assoc()) {print_r($row["foto"]);}?>" class="" style="height: 150px; width: 150px"
-                >
-            </div>
-        </div>
-        <div class="row">
             <div class="col-md-6">
-                <form method="post" action="php/update.php" id="formupdate">
+                <h1 style="font-size: 30px">Carlo</h1>
+                <img src="<?php while($row = $resfoto->fetch_assoc()) {print_r($row["foto"]);}?>" class="" style="height: 150px; width: 150px">
+                <form class="mt-2" method="post" action="php/update.php" id="formupdate">
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Email</label>
                                 <input
-                                 type="text" name="mail" class="form-control"
-                                 value="<?php while($row = $resemail->fetch_assoc()) {print_r($row["email"]);}?>"
-                                 >
+                                        type="text" name="mail" class="form-control"
+                                        value="<?php while($row = $resemail->fetch_assoc()) {print_r($row["email"]);}?>"
+                                >
                             </div>
                             <div class="form-group">
                                 <label>Nickname</label>
                                 <input
-                                 type="text" name="nickname" class="form-control"
-                                 value="<?php while($row = $resnickname->fetch_assoc()) {print_r($row["nickname"]);}?>"
+                                        type="text" name="nickname" class="form-control"
+                                        value="<?php while($row = $resnickname->fetch_assoc()) {print_r($row["nickname"]);}?>"
                                 >
                             </div>
                             <div class="form-group">
                                 <label>Password</label>
-                                <input type="password" name="password" class="form-control" 
-                                value="">
+                                <input type="password" name="password" class="form-control"
+                                       value="">
                             </div>
                             <div class="form-group">
                                 <label>City</label>
-                                <input type="text" name="citta_n" class="form-control" 
-                                value=
-                                <?php while($row = $rescitta_n->fetch_assoc()) {
-                          print_r($row["citta_n"]);
-                           }?>   >
+                                <input type="text" name="citta_n" class="form-control"
+                                       value=
+                                       <?php while($row = $rescitta_n->fetch_assoc()) {
+                                           print_r($row["citta_n"]);
+                                       }?>   >
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Date of birth</label>
-                                <input type="date" name="data_n" class="form-control" 
-                                value="<?php while($row = $resdata_n->fetch_assoc()) {
-                                    print_r($row["data_n"]);
-                                }?>">
+                                <input type="date" name="data_n" class="form-control"
+                                       value="<?php while($row = $resdata_n->fetch_assoc()) {
+                                           print_r($row["data_n"]);
+                                       }?>">
                             </div>
                             <div class="form-group">
                                 <label>Genre</label>
-                                <select name="sesso" class="form-control" 
-                                placeholder=
-                                <?php while($row = $ressesso->fetch_assoc()) {
-                          print_r($row["sesso"]);
-                           }?>   >
+                                <select name="sesso" class="form-control"
+                                        placeholder=
+                                        <?php while($row = $ressesso->fetch_assoc()) {
+                                            print_r($row["sesso"]);
+                                        }?>   >
                                     <option value="m">Male</option>
                                     <option value="f">Female</option>
                                 </select>
@@ -129,7 +124,50 @@
                             </div>
                             <button type="submit" name="update" class="btn btn-outline-primary my-5" style="float: right">Modify</button>
                         </div>
+                    </div>
                 </form>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <h1 style="font-size: 30px">My friends</h1>
+                </div>
+                <!-- template for friend's list-->
+                <div class="row mb-4" id="friend-template" style="text-align: center">
+                    <div class="col-2">
+                        <img class="card-img-top" src="..." alt="Card image cap" style="width: 50px; height: auto">
+                    </div>
+                    <div class="col-8">
+                        <h5>Piero Leddi</h5>
+                        <small>Here friendship info</small>
+                    </div>
+                    <div class="col-2">
+                        <button class="btn btn-light btn-sm"><i class="far fa-trash-alt"></i></button>
+                    </div>
+                </div>
+                <div class="row mb-4" id="friend-template" style="text-align: center">
+                    <div class="col-2">
+                        <img class="card-img-top" src="..." alt="Card image cap" style="width: 50px; height: auto">
+                    </div>
+                    <div class="col-8">
+                        <h5>Lorenzo Gandini</h5>
+                        <small>Here friendship info</small>
+                    </div>
+                    <div class="col-2">
+                        <button class="btn btn-light btn-sm"><i class="far fa-trash-alt"></i></button>
+                    </div>
+                </div>
+                <div class="row mb-4" id="friend-template" style="text-align: center">
+                    <div class="col-2">
+                        <img class="card-img-top" src="..." alt="Card image cap" style="width: 50px; height: auto">
+                    </div>
+                    <div class="col-8">
+                        <h5>Andrea Cerizza</h5>
+                        <small>Here friendship info</small>
+                    </div>
+                    <div class="col-2">
+                        <button class="btn btn-light btn-sm"><i class="far fa-trash-alt"></i></button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
